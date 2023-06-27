94e5041d18e336cfe03725984376b9e0a2425667
Nathan Seva <thykof@protonmail.ch>
2023-06-27 10:25:37 +0200

    880 Nathan (#892)
    * add endpoint to enable updateNdeURL
    
    * fix swagger
    
    * draft
    
    * add configuration file for different networks
    
    * add needed config
    
    * use configuration yaml file and implement logique to switch between networks
    
    * split the config from network code
    
    * best practice not init no global var
    
    * refactor code
    
    * clean config package and
    
    * use interface for network manager
    
    * add yaml file for network config
    
    * update config file
    
    * fix swagger to handle correctly switch and get networks
    
    * fix swagger to handle correctly switch and get networks
    
    * remove useless function
    
    * implement new api endpoint
    
    * add implementation for networkmanager endpoints
    
    * add enum for possible networks
    
    * split and document handlers
    
    * more apropriate naming
    
    * fumpt
    
    * bad naming
    
    * embed and refactor
    
    * move conf network to conf direcgtory
    
    * lint
    
    * lint
    
    * buildnet s default
    
    * remove useless interface
    
    * useless mutex in get
    
    * gofumpt
    
    * adapt to new naming
    
    * error code
    
    * add mirage mock for get endpoint, and dependdencies
    
    * extract network options and add to front
    
    * handle on change network in front
    
    ---------
    
    Co-authored-by: BRAHIMI Naoufal <nb@massa.net>

--- web/massastation/src/layouts/LayoutStation/LayoutStation.tsx
+++ web/massastation/src/layouts/LayoutStation/LayoutStation.tsx
@@ -1,11 +1,8 @@
 import { ReactNode, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { routeFor } from '../../utils';
-
-import { useResource } from '../../custom/api';
-import { AccountObject } from '../../models/AccountModel';
-import { useAccountStore } from '../../store/store';
+import { Dropdown, StationLogo, ThemeMode } from '@massalabs/react-ui-kit';
 import { URL } from '../../const/url/url';
+import { useResource } from '../../custom/api';
+import { NetworksObject } from '../../models/AccountModel';
 
 import { PAGES } from '../../const/pages/pages';
 
@@ -22,67 +19,58 @@ import { MASSA_WALLET } from '../../const/const';
 
 export interface LayoutStationProps {
   children?: ReactNode;
-  navigator?: Navigator;
   onSetTheme?: () => void;
-  storedTheme?: string;
   activePage: string;
 }
 
-export function LayoutStation({ ...props }) {
-  const { children, navigator, onSetTheme, storedTheme, activePage } = props;
-
-  const navigate = useNavigate();
-
-  const [selectedTheme, setSelectedTheme] = useState(
-    storedTheme || 'theme-dark',
-  );
-
-  const searchIsActive = activePage === PAGES.SEARCH;
+export function LayoutStation({
+  children,
+  onSetTheme,
+  activePage,
+}: LayoutStationProps) {
+  const [selectedTheme, setSelectedTheme] = useState('theme-dark');
+  const [availableNetworks, setAvailableNetworks] = useState([]);
+  const [currentNetwork, setCurrentNetwork] = useState('');
 
   function handleSetTheme(theme: string) {
     setSelectedTheme(theme);
-
     onSetTheme?.(theme);
   }
 
-  const { data: accounts = [] } = useResource<AccountObject[]>(
-    `${URL.WALLET_BASE_API}/${URL.WALLET_ACCOUNTS}`,
+  const { data: networkConfig = {} } = useResource<NetworksObject>(
+    `${URL.PATH_NETWORKS}`,
   );
-
-  const currentAccount = useAccountStore((state) => state.currentAccount);
-  const setCurrentAccount = useAccountStore((state) => state.setCurrentAccount);
-
-  const accountsItems = accounts.map((account) => ({
-    icon: <Identicon username={account.nickname} size={32} />,
-    item: account.nickname,
-    onClick: () => setCurrentAccount(account.nickname),
-  }));
-
-  const selectedAccountKey: number = parseInt(
-    Object.keys(accounts).find(
-      (_, idx) => accounts[idx].nickname === currentAccount,
-    ) || '0',
-  );
-
-  const existingAccount: boolean = accounts.length > 0;
-
-  const [pluginWalletIsInstalled, setPluginWalletIsInstalled] = useState(false);
-
-  const { data: plugins, isSuccess } =
-    useResource<IMassaStore[]>('plugin-manager');
 
   useEffect(() => {
-    if (isSuccess) {
-      setPluginWalletIsInstalled(
-        plugins.some((plugin) => plugin.name === MASSA_WALLET),
+    if (networkConfig.availableNetworks) {
+      setAvailableNetworks(
+        networkConfig.availableNetworks.map((n) => ({ item: n })),
       );
     }
-  }, [isSuccess, plugins]);
+    if (networkConfig.currentNetwork) {
+      setCurrentNetwork(networkConfig.currentNetwork);
+    }
+  }, [networkConfig]);
+
+  const handleNetworkChange = (selectedNetwork: string) => {
+    // Make the network switch request here
+    // Assuming you are using fetch() for API requests
+    fetch(`/network/${selectedNetwork}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response if necessary
+        console.log('Current Network:', data.currentNetwork);
+      })
+      .catch((error) => {
+        // Handle the error if necessary
+        console.error(error);
+      });
+  };
 
   return (
     <div
       data-testid="layout-station"
-      className={`min-h-screen bg-primary px-20 pt-12 pb-8 $}`}
+      className="min-h-screen bg-primary px-20 pt-12 pb-8"
     >
       <div className="grid grid-cols-3">
         <div className="flex justify-start">
@@ -90,40 +78,15 @@ export function LayoutStation({ ...props }) {
             <StationLogo theme={selectedTheme} />
           </a>
         </div>
-        <div className="flex justify-center">
-          {navigator && <div className="flex-row-reversed">{navigator}</div>}
-        </div>
+        <div className="flex justify-center"></div>
         <div className="flex justify-end items-start gap-20">
-          {searchIsActive &&
-            (pluginWalletIsInstalled ? (
-              existingAccount ? (
-                <div className="w-64">
-                  <Dropdown
-                    options={accountsItems}
-                    select={selectedAccountKey}
-                  />
-                </div>
-              ) : (
-                <Button
-                  customClass="w-64"
-                  onClick={() =>
-                    window.open(
-                      '/plugin/massa-labs/massa-wallet/web-app/',
-                      '_blank',
-                    )
-                  }
-                >
-                  {Intl.t('search.buttons.create-account')}
-                </Button>
-              )
-            ) : (
-              <Button
-                customClass="w-64"
-                onClick={() => navigate(routeFor('index'))}
-              >
-                {Intl.t('search.buttons.install-wallet')}
-              </Button>
-            ))}
+          <div className="w-64">
+            <Dropdown
+              options={availableNetworks}
+              select={currentNetwork}
+              onChange={handleNetworkChange}
+            />
+          </div>
           <ThemeMode onSetTheme={handleSetTheme} />
         </div>
       </div>
